57e97e6cdda937e8be96ae24ab85fdbe5e8a7258
SHALINIMOURALY <126889011+SHALINIMOURALY@users.noreply.github.com>
2024-03-05 15:44:33 +0530

    its is a basic program

---
+++ corejava/src/day1/Sample.java
@@ -0,0 +1,22 @@
+package day1;
+import java.util.*;
+
+public class Sample {
+
+		
+		public static void main(String[] args)
+		{
+			Scanner s = new Scanner(System.in);
+			a = s. nextInt();
+			b = s. nextInt();
+			c = a-b;
+			System.out.println("subtraction of two number:"+ c);
+			System.out.println("Hello world");
+			System.out.println("by shalu");
+			int a,b,c;
+			a=100;
+			b=20;
+			c=a+b;
+			System.out.println("addition of two number:"+ c);
+		}
+}
---
+++ corejava/src/day1/Sample1.java
@@ -0,0 +1,22 @@
+
+package day1;
+
+import java.util.Scanner;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner s = new Scanner(System.in);
+		int a,b,c;
+		System.out.println("enter the two number:");
+		a = s. nextInt();
+		b = s. nextInt();
+		c = a-b;
+		
+		System.out.println("subtraction of two number:"+c);
+
+	}
+
+}
