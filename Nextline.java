1002d7a5a4d4c203f8e9ed9e0368b533782b5073
SHALINIMOURALY <126889011+SHALINIMOURALY@users.noreply.github.com>
2024-03-11 18:25:06 +0530

    nextline

---
+++ Nextline.java
@@ -0,0 +1,15 @@
+package Day3;
+import java.util.*;
+
+public class Nextline {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the name");
+		String b =  input.nextLine();
+		System.out.println("Hello" +b+ "coders");
+		
+
+	}
+
+}
